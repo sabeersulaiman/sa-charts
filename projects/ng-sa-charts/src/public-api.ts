511d536c7c8ec2908801eee3065e8defa8c1f77a
Sabeer Sulaiman <sabeersulaimanpv@gmail.com>
2020-03-18 08:55:39 +0530

    added main projects

---
+++ projects/ng-sa-charts/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-sa-charts
+ */
+
+export * from './lib/ng-sa-charts.service';
+export * from './lib/ng-sa-charts.component';
+export * from './lib/ng-sa-charts.module';
